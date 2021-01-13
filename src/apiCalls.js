7dc1be9d6b5d4fe468c2aa95f77263ec65169264
Bailey Dunning <bwdunning@gmail.com>
2021-01-12 22:21:16 -0700

    Add other method for fetching unique plant info

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,3 +4,10 @@ export const getPlantList = () => {
     .then(data => data.data)
   )
 }
+
+export const getPlantDetails = (endpoint) => {
+  return (
+    fetch(`https://trefle.io${endpoint}?token=w76udTztX_89MySv3fO4fG41HD2yq9xhIRETq1KCXCg`)
+      .then(data => data.data)
+  )
+}
