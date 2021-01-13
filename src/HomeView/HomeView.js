25af6154bb5bc2ada6015a1c15d867ad21c010c1
Bailey Dunning <bwdunning@gmail.com>
2021-01-12 22:49:04 -0700

    Add basic render to ToggleCardView, CardContainer and PlantCard

--- src/HomeView/HomeView.js
+++ src/HomeView/HomeView.js
@@ -1,11 +1,15 @@
 import Header from '../Header/Header'
 import SearchContainer from '../SearchContainer/SearchContainer'
+import ToggleCardView from '../ToggleCardView/ToggleCardView'
+import CardContainer from '../CardContainer/CardContainer'
 
 const HomeView = () => {
   return (
     <section>
       <Header />
       <SearchContainer />
+      <ToggleCardView />
+      <CardContainer />
     </section>
   )
 }
---
+++ src/ToggleCardView/ToggleCardView.js
@@ -0,0 +1,10 @@
+const ToggleCardView = () => {
+  return (
+    <nav>
+      <button>All</button>
+      <button>Favorites</button>
+    </nav>
+  )
+}
+
+export default ToggleCardView
