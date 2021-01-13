93ac53f1c716bf681737f5a881f718d43f505000
Bailey Dunning <bwdunning@gmail.com>
2021-01-13 10:36:27 -0700

    Add hooks to HomeView

--- src/HomeView/HomeView.js
+++ src/HomeView/HomeView.js
@@ -1,8 +1,18 @@
+import React, { useEffect, useState } from 'react'
 import Header from '../Header/Header'
 import ToggleCardView from '../ToggleCardView/ToggleCardView'
 import CardContainer from '../CardContainer/CardContainer'
+import { getPlantList } from '../apiCalls'
 
 const HomeView = () => {
+  const [plantList, setPlantList] = useState([])
+
+  // useEffect(() => {
+  //   getPlantList()
+  //   .then(data => setPlantList(data.data))
+  //   .catch(error => console.log(error))
+  // }, [plantList])
+
   return (
     <section>
       <Header />
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,13 +1,14 @@
-export const getPlantList = () => {
-  return (
-    fetch('https://trefle.io/api/v1/plants?token=w76udTztX_89MySv3fO4fG41HD2yq9xhIRETq1KCXCg')
-    .then(data => data.data)
+export const getPlantList = async () => {
+  const response = await fetch(
+    'https://trefle.io/api/v1/plants?token=w76udTztX_89MySv3fO4fG41HD2yq9xhIRETq1KCXCg'
   )
+  const data = await response.json()
+  return data
 }
 
-export const getPlantDetails = (endpoint) => {
-  return (
-    fetch(`https://trefle.io${endpoint}?token=w76udTztX_89MySv3fO4fG41HD2yq9xhIRETq1KCXCg`)
-      .then(data => data.data)
-  )
-}
+// export const getPlantDetails = (endpoint) => {
+//   return (
+//     fetch(`https://trefle.io${endpoint}?token=w76udTztX_89MySv3fO4fG41HD2yq9xhIRETq1KCXCg`)
+//       .then(response => response.json())
+//   )
+// }
