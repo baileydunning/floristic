69e23447c4ef2331db988a0dac0c0f8cd3802cac
Bailey Dunning <bwdunning@gmail.com>
2021-01-13 15:27:42 -0700

    Make PlantCards flip boxes

--- src/CardContainer/PlantCard/PlantCard.js
+++ src/CardContainer/PlantCard/PlantCard.js
@@ -1,7 +1,16 @@
+import './PlantCard.scss'
+
 const PlantCard = ({ plant }) => {
   return (
-    <div>
-      <p>{ plant['common_name'] }</p>
+    <div className="flip-box">
+      <div className="flip-box-inner">
+        <div className="flip-box-front">
+          <img src={ plant['image_url'] } alt='plant-img'/>
+        </div>
+        <div className="flip-box-back">
+          <h3>{ plant['common_name'] }</h3>
+        </div>
+      </div>
     </div>
   )
 }
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,8 @@
+// const url = 'http://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants?token=w76udTztX_89MySv3fO4fG41HD2yq9xhIRETq1KCXCg'
+
 export const getPlantList = async () => {
-  const url = 'http://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants?token=w76udTztX_89MySv3fO4fG41HD2yq9xhIRETq1KCXCg'
+  const url = 'http://localhost:8080/https://trefle.io/api/v1/plants?token=w76udTztX_89MySv3fO4fG41HD2yq9xhIRETq1KCXCg'
   const response = await fetch(url)
   const data = await response.json()
-  return data 
+  return data
 }
