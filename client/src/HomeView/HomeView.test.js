a13ab8f238b210f7cac3ccaa4afe104a9e9c0297
Bailey Dunning <bwdunning@gmail.com>
2021-01-18 14:49:34 -0700

    Test state in HomeView

--- client/src/HomeView/HomeView.test.js
+++ client/src/HomeView/HomeView.test.js
@@ -8,43 +8,65 @@ import userEvent from '@testing-library/user-event'
 import { samplePlantList, sampleFavorites, samplePlantData } from '../testData'
 import '@testing-library/jest-dom'
 import { getPlantList } from '../apiCalls'
-import { homeReducer, initialState } from './HomeReducer'
+import { homeReducer } from './HomeReducer'
 
 
 describe('HomeView', () => {
+  const mockState = {
+    plantList: samplePlantList.data,
+    links: samplePlantList.links,
+    pageNumber: 1,
+    view: 'all'
+  }
+  const { result } = renderHook(() => useReducer(homeReducer, mockState))
+  const [state, dispatch] = result.current
+  
   const history = createMemoryHistory()
-  const mockFetchData = jest.fn()
-  // const mockFetchLinks = jest.fn()
-  // const mockJumpToPage = jest.fn()
-  // const mockToggleView = jest.fn()
+  const mockFetchData = jest.fn().mockImplementation(() => {
+    state.plantList = samplePlantList.data
+  })
+  const mockFetchLinks = jest.fn().mockImplementation(() => {
+    state.links = samplePlantList.links
+  })
+  const mockJumpToPage = jest.fn()
+  const mockToggleView = jest.fn()
+  
   
   jest.mock('../apiCalls')
   jest.mock('./HomeReducer.js', () => ({
-    FETCH_DATA: mockFetchData
+    FETCH_DATA: () => mockFetchData(),
+    FETCH_LINKS: () => mockFetchLinks(),
+    JUMP_TO_PAGE: () => mockJumpToPage(),
+    TOGGLE_VIEW: () => mockToggleView()
   }))
   
-  // const { result } = renderHook(() => useReducer(homeReducer, initialState))
-  // const [state, dispatch] = result.current
   
   beforeEach(async () => {
-    // getPlantList.mockResolvedValue(() => samplePlantList)
-    mockFetchData.mockImplementation(() => {
-      return state.plantList = samplePlantList.data
-    })
-   
+
     await act(async () => {
       render(
         <Router history={history}>
           <HomeView />
         </Router>
       )
-
-      // dispatch({type: 'FETCH_DATA', plantList: samplePlantList.data})
     })
+  })
+
+  it('should hold state in reducer', () => {
+    expect(state.plantList).toBeTruthy()
+    expect(state.plantList.length).toBe(4)
+    expect(state.links).toBeTruthy()
+    expect(state.view).toBe('all')
+    expect(state.pageNumber).toBe(1)
+  })
+
+  it('should dispatch actions', () => {
+
   })
   
   it('should render the home page', async () => {
-    
+    const header = screen.getByText('floristic')
+    expect(header).toBeInTheDocument()
     screen.debug()
   })
 })
