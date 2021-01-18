59fab13bfa5b85b5fbb10ba297ae3f075f457256
Bailey Dunning <bwdunning@gmail.com>
2021-01-17 19:02:59 -0700

    Set up footer test and mock context

--- client/src/HomeView/Footer/Footer.test.js
+++ client/src/HomeView/Footer/Footer.test.js
@@ -2,48 +2,29 @@ import Footer from './Footer'
 import HomeContext from '../HomeContext'
 import { screen, render } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import '@testing-library/jest-dom/extend-expect'
 import userEvent from '@testing-library/user-event'
-import { Router } from 'react-router-dom'
-import { createMemoryHistory } from 'history'
+
 
 describe('Footer', () => {
-  let nextPage
-  let prevPage
 
-  const customRender = (ui, { providerProps, ...renderOptions }) => {
-    return render(
-      <HomeContext.Provider value={providerProps}>{ui}</HomeContext.Provider>,
-      renderOptions
+  beforeEach(() => {
+    const mockContext = { pageNumber: 1 }
+    let determineMaxPage = jest.fn()
+    let jumpToPage = jest.fn()
+    determineMaxPage.mockImplementation(() => '5')
+
+    render(
+      <HomeContext.Provider value={mockContext}>
+        <Footer
+          determineMaxPage={determineMaxPage}
+          jumpToPage={jumpToPage}
+        />
+      </HomeContext.Provider>
     )
-  }
-
-
-  // beforeEach(() => {
-  //   render(
-  //       <HomeContext.Consumer>
-  //         <Footer />
-  //       </HomeContext.Consumer>
-  //   )
-
-  //   nextPage = screen.getByText('Next page')
-  //   prevPage = screen.getByText('Previous page')
-  // })
-
-  it('should have a default page number from Provider', () => {
-    const providerProps = {
-      pageNumber: 1
-    }
-
-    customRender(<Footer />, providerProps)
-    screen.debug()
-
   })
-
-  it('should render the footer', () => {
-  })
-
-  it('should redirect to a new url', () => {
-    
+  
+  it('should mock the context', () => {
+    const pageNumber = screen.getByText('1')
+    expect(pageNumber).toBeInTheDocument()
   })
 })
