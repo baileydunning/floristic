89efeb1147934628e0ce16b3892bf78bed1113b7
Bailey Dunning <bwdunning@gmail.com>
2021-03-30 20:43:43 -0600

    update api url

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,19 +1,19 @@
 export const getPlantList = async (page) => {
-  const url = `http://localhost:3001/plants/${page}`
+  const url = `https://floristic-api.herokuapp.com/plants/${page}`
   const response = await fetch(url)
   const data = await response.json()
   return data
 }
 
 export const getPlant = async (id) => {
-  const url = `http://localhost:3001/plant/${id}`
+  const url = `https://floristic-api.herokuapp.com/plant/${id}`
   const response = await fetch(url)
   const data = await response.json()
   return data
 }
 
 export const searchPlants = async (query, page) => {
-  let url = `http://localhost:3001/plants/search/${query}/${page}`
+  let url = `https://floristic-api.herokuapp.com/plants/search/${query}/${page}`
   const response = await fetch(url)
   const data = await response.json()
   return data
