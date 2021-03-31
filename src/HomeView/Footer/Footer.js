6ff4e1ad93831af0619fd248eac10b3706c54944
Bailey Dunning <bwdunning@gmail.com>
2021-03-30 21:27:57 -0600

    add credits

--- src/HomeView/Footer/Footer.js
+++ src/HomeView/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = ({ pageNumber, maxPage, jumpToPage }) => {
   const [maximum, setMaximum] = useState('10')
   const [pageInput, setPageInput] = useState(null)
 
-  useEffect(() => {  
+  useEffect(() => {
     if (maxPage.includes('&q')) {
       let newMax = maxPage.split('&')[0]
       setMaximum(+newMax)
@@ -21,34 +21,40 @@ const Footer = ({ pageNumber, maxPage, jumpToPage }) => {
   }
 
   return (
-    <footer className='footer' data-testid='footer'>
-      { +pageNumber > 1 && 
-        <button 
-          onClick={() => jumpToPage(+pageNumber - 1)}>
-          ←
+    <section>
+      <footer className='footer' data-testid='footer'>
+        {+pageNumber > 1 &&
+          <button
+            onClick={() => jumpToPage(+pageNumber - 1)}>
+            ←
         </button>
-      }
-      <form 
-        onSubmit={e => submitPage(e)}
-        className="page-form"
-      >
-        <input
-          type="number"
-          className="page-input"
-          placeholder={ pageNumber }
-          value={ pageInput }
-          min={ 0 }
-          max={ maximum }
-          onChange={e => setPageInput(e.target.value)}
-        />
-      </form>
-      { +pageNumber < maximum && 
-        <button 
-          onClick={() => jumpToPage(+pageNumber + 1)}>
-          →
+        }
+        <form
+          onSubmit={e => submitPage(e)}
+          className="page-form"
+        >
+          <input
+            type="number"
+            className="page-input"
+            placeholder={pageNumber}
+            value={pageInput}
+            min={0}
+            max={maximum}
+            onChange={e => setPageInput(e.target.value)}
+          />
+        </form>
+        {+pageNumber < maximum &&
+          <button
+            onClick={() => jumpToPage(+pageNumber + 1)}>
+            →
         </button>
-      }
-    </footer>
+        }
+      </footer>
+      <div className='footer-credits'>
+        <a href='https://github.com/baileydunning' target='_blank'>developed by Bailey Dunning</a>
+        <a href='https://docs.trefle.io/reference/' target='_blank'>data provided by Trefle REST API</a>
+      </div>
+    </section>
   )
 }
 
