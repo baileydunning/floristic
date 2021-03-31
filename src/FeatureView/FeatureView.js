d70512fea4ca870c4e172c2c233fbdfe93e82864
Bailey Dunning <bwdunning@gmail.com>
2021-03-30 20:59:04 -0600

    add modal and classes

--- src/FeatureView/FeatureView.js
+++ src/FeatureView/FeatureView.js
@@ -26,38 +26,90 @@ const FeatureView = ({ id }) => {
     }
   }
 
+  const openModal = () => {
+    const modal = document.getElementById('modal')
+    modal.style.display = 'block'
+  }
+
+  const closeModal = () => {
+    const modal = document.getElementById('modal')
+    modal.style.display = 'none'
+  }
+
   return (
     <section className='feature' data-testid='feature-view'>
       {plantData ?
         <section>
-          <h1>{plantData['scientific_name']}:</h1>
-          <h2>{plantData['common_name']} from the {plantData['family_common_name']}</h2>
-          <h3>Genus: {plantData.genus.name}</h3>
-          <h3>Family: {plantData.family.name}</h3>
-          <p><b>Observations: </b>{plantData.observations}</p>
-          <div className='distributions'>
-            <div className='native'>
-              <p><b>Native: </b></p>
-              <ul>
-                {listLocations('native')}
-              </ul>
+          <section className='title-container'>
+            <Link to='/'>
+              <img
+                src='https://img.icons8.com/pastel-glyph/2x/circled-left.png'
+                alt='go-back-btn'
+                className='go-back'
+              />
+            </Link>
+            <div>
+              <h1 className='header-feature'>{plantData['scientific_name']}:</h1>
+              <h2>{plantData['common_name']} from the {plantData['family_common_name']}</h2>
             </div>
-            <div className='introduced'>
-              <p><b>Introduced: </b></p>
-              <ul>
-                {listLocations('introduced')}
-              </ul>
+          </section>
+          <section className='table-img'>
+            <img src={plantData['image_url']} />
+            <table className='table'>
+              <tr>
+                <th>Genus</th>
+                <td>{plantData.genus.name}</td>
+              </tr>
+              <tr>
+                <th>Family</th>
+                <td>{plantData.family.name}</td>
+              </tr>
+              <tr>
+                <th>Year</th>
+                <td>{plantData.year}</td>
+              </tr>
+              <tr>
+                <th>Status</th>
+                <td>{plantData['main_species'].status}</td>
+              </tr>
+              <tr>
+                <th>Edible</th>
+                {plantData['main_species'].edible ? <td>✓</td> : <td>✘</td>}
+              </tr>
+              <tr>
+                <th>Vegetable</th>
+                {plantData['main_species'].vegetable ? <td>✓</td> : <td>✘</td>}
+              </tr>
+              <tr>
+                <th
+                  className='observations'
+                  onClick={() => openModal()}
+                >
+                  Observations
+                </th>
+                <td>{plantData.observations}</td>
+              </tr>
+            </table>
+          </section>
+          <section id='modal' onClick={() => closeModal()}>
+            <div className='modal-content'>
+              <div className='native'>
+                <p><b>Native: </b></p>
+                <ul>
+                  {listLocations('native')}
+                </ul>
+              </div>
+              <div className='introduced'>
+                <p><b>Introduced: </b></p>
+                <ul>
+                  {listLocations('introduced')}
+                </ul>
+              </div>
             </div>
-          </div>
-          <div>
-            {plantData['main_species'].edible ? <p>✓ Edible</p> : <p>✘ Inedible</p>}
-            {plantData['main_species'].vegetable ? <p>✓ Vegetable</p> : <p>✘ Not a Vegetable</p>}
-          </div>
-          <img src={plantData['image_url']} />
+          </section>
         </section> :
         <Loading />
       }
-      <Link to='/'>Go back</Link>
     </section>
   )
 }
